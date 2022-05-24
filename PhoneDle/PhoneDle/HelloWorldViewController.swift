7452aec99c08b07863477425b7a6fa6b5caa960f
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-05-24 22:54:06 +0900

    ,

---
+++ PhoneDle/PhoneDle/HelloWorldViewController.swift
@@ -0,0 +1,42 @@
+//
+//  HelloWorldViewController.swift
+//  PhoneDle
+//
+//  Created by Jin younkyum on 2022/05/24.
+//
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var worldLabel: UILabel!
+    @IBOutlet weak var helloSegment: UISegmentedControl!
+    @IBOutlet weak var worldSegment: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        helloLabel.textAlignment = .center
+        worldLabel.textAlignment = .right
+        
+        helloSegment.selectedSegmentIndex = helloLabel.textAlignment.rawValue
+        worldSegment.selectedSegmentIndex = worldLabel.textAlignment.rawValue
+    }
+    
+    @IBAction func helloSegmentChanged(_ sender: UISegmentedControl) {
+        helloLabel.textAlignment = NSTextAlignment(rawValue: sender.selectedSegmentIndex) ?? .center
+        isClaer()
+    }
+    
+    @IBAction func worldSegmentChanged(_ sender: UISegmentedControl) {
+        worldLabel.textAlignment = NSTextAlignment(rawValue: sender.selectedSegmentIndex) ?? .center
+        isClaer()
+    }
+    
+    func isClaer() {
+        if helloLabel.textAlignment.rawValue == 2 && worldLabel.textAlignment.rawValue == 0 {
+            print("clear")
+        }
+    }
+}
