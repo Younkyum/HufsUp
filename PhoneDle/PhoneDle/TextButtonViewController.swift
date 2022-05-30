9a4f70e26bcf5539d78be0da7f4a69a8f9bbb9c0
YounKyum Jin <jinyounkyum@hufs.ac.kr>
2022-05-31 04:31:26 +0900

    textbutton

---
+++ PhoneDle/PhoneDle/TextButtonViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TextButtonViewController.swift
+//  PhoneDle
+//
+//  Created by Jin younkyum on 2022/05/31.
+//
+
+import UIKit
+
+class TextButtonViewController: UIViewController {
+    
+    var touchCount = 0
+    @IBOutlet weak var HelloButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        clearButton.isEnabled = false
+        clearButton.alpha = 0
+        HelloButton.setTitle("FAKE \(touchCount)", for: .highlighted)
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func fakeTouched(_ sender: Any) {
+        touchCount += 1
+        HelloButton.setTitle("FAKE \(touchCount)", for: .highlighted)
+        if touchCount >= 10 {
+            clearButton.alpha = 1
+            clearButton.isEnabled = true
+        }
+    }
+    @IBAction func clearButtonPushed(_ sender: Any) {
+        print("clear")
+    }
+    
+}
